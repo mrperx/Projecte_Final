3f75ff1e430a6b8d0982178924538c372e8a656c
CodeOpFE <codeopfe@MacBook-Air-de-CodeOpFE-2.local>
2022-04-04 13:28:56 +0200

    inici

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,15 @@
-<template></template>
+<template>
+  <h1 class="border bg-slate-400">APP</h1>
+</template>
 
-<script></script>
+<script>
+import Home from "./components/Home.vue";
+import NavBar from "./components/NavBar.vue";
+export default {
+  name: "App",
+  components: {
+    home: Home,
+    navBar: NavBar,
+  },
+};
+</script>
---
+++ src/components/routes.js
@@ -0,0 +1,7 @@
+import Home from "./components/Home.vue";
+import NavBar from "./components/NavBar.vue";
+
+export const routes = [
+  { path: "/", name: "home", component: Home },
+  { path: "/navbar", name: "NaVBAR", component: NavBar },
+];
