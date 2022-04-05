f17b6b55a8ee9265ffafdd6d4dc0e9ea0a649489
mrperx <mribasperxachs@gmail.com>
2022-04-05 18:37:18 +0200

    wp

--- src/components/SearchRecipe.vue
+++ src/components/SearchRecipe.vue
@@ -1,5 +1,31 @@
 <template></template>
 
 <script>
-export default {};
+import axios from "axios";
+export default {
+  data() {
+    return {
+      data: null,
+      loading: false,
+      error: null,
+    };
+  },
+  methods: {
+    async sendApiRequest() {
+      this.loading = true;
+      const APP_ID = "38ba7739";
+      const Api_Key = "8567c4564eecc301097726808737edb6";
+      try {
+        const { data } = await axios(
+          `https://api.edamam.com/api/search?app_id=${APP_ID}&app_key=${Api_Key}`
+        );
+        this.data = data;
+      } catch (error) {
+        this.error = error;
+      } finally {
+        this.loading = false;
+      }
+    },
+  },
+};
 </script>
