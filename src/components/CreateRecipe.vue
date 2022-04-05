eec957d8b6d197b28f810d7ed3af86b0a6d3de79
CodeOpFE <codeopfe@MacBook-Air-de-CodeOpFE-2.local>
2022-04-05 13:05:12 +0200

    wp

--- src/components/CreateRecipe.vue
+++ src/components/CreateRecipe.vue
@@ -1,15 +1,27 @@
 <template>
-  <div class="felx container">
-    <h2
-      class="text-center italic font-semibold text-lime-500 shadow-gray-400 font-serif text-xl"
-    >
-      Create Recipe
-    </h2>
-    <div class="felx">
-      <h4>Title :</h4>
-      <input class="border" />
+  <h2
+    class="text-center italic font-semibold text-lime-500 shadow-gray-400 font-serif text-xl"
+  >
+    Create Recipe
+  </h2>
+
+  <div class="grid grid-cols-2 container bg-cyan-200 p-3">
+    <div>
+      <img />
+      <h4>Title : <input class="border" /></h4>
+      <h4>
+        Ingredients:
+        <div class="flex">
+          Quantitat :<input class="border" />
+          <textarea class="border">Ingredients...</textarea>
+        </div>
+      </h4>
     </div>
-    <textarea class="border">Process...</textarea>
+
+    <div class="items-center">
+      <h4>Process</h4>
+      <textarea class="border">Process...</textarea>
+    </div>
   </div>
 </template>
 
